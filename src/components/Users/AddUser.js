c37779337e44d6d891a2cce1961e93de0b4739db
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-10-30 12:25:57 +0000

    feat: Resetting form fields after Submit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card"
 import clases from  "./AddUser.module.css";
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        console.log(event);
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
+
+    const usernameChangeHandler = (event) =>{
+        setEnteredUsername(event.target.value);        
+    }
+    const ageChangeHandler = (event) =>{        
+        setEnteredAge(event.target.value);
+    }    
 
     return (
         <Card className={clases.input}>
             <form onSubmit={addUserHandler}>
                 
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
                                
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
                
                 <Button type="submit">Add User</Button>
                 
