0c6e6a059c42bd7c8a92a3efa3303e8cbf64a7df
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-10-30 13:48:14 +0000

    feat: creating UsersList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 
 
 function App() {
+
+  const users = [];
+
   return (
     <div>
       <AddUser></AddUser>
+      <UsersList users={users}></UsersList>
     </div>
   );
 }
